81ca2e51e233ad7aa6e15f2743059498ba1abe24
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:38:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,7 +27,6 @@ end
 def assign_rooms(attendees)
   attendees.each_with_index.map do | attendee, index |
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-#index <= 7
 end
 end
 #def square_array(array)
