bf7250892ab6339e700c06c9e8dd2d1249ac71d1
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:06:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
 # Write your code here.]
-def badge_maker(name)
-  name.each do
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(names)
+  names.each do
     puts "Hello, my name is '{name}'."
 end
