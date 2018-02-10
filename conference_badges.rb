04f45b27ee97e07c23bac3b3c0cbc2feaa4fbde2
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:47:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,20 +1,14 @@
 # Write your code here.]
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
-#names.each do |name|
    "Hello, my name is #{name}."
-  #attendees.each {|attendee| puts "Hello, my name is #{name}."  }
-    #puts "Hello, my name is '{name}'."
-  #  names
 end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}."
   end
-
 end
-
 
 #Write a method called assign_rooms that takes the
 #list of speakers and assigns each speaker to a room.
@@ -35,24 +29,8 @@ end
 def printer(attendees)
  batch_badge_creator(attendees).each do |badge|
     puts badge
-  #assign_rooms.each do | attendee, index |
-  #print assign_rooms
-end
+  end
   assign_rooms(attendees).each do |assignment|
     puts assignment
 end
 end
-
-#def printer(assign_rooms)
-#  assign_rooms(attendees).each do |assignment|
-
-#    assign_rooms(attendees).each do |assignment|
-#    puts assign_rooms
-
-#  end
-#end
-#def square_array(array)
-#  squared = []
-#  array.each { |number| squared << number **2 }
-  #squared
-#end
