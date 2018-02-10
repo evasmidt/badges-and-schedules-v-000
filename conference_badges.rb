11e225c68b249d33da86b0ba791166d7de5339ea
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:21:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
 #names.each do |name|
-  #puts "Hello, my name is #{name}."
-  attendees.each {|attendee| puts "Hello, my name is #{name}."  }
+   "Hello, my name is #{name}."
+  #attendees.each {|attendee| puts "Hello, my name is #{name}."  }
     #puts "Hello, my name is '{name}'."
   #  names
 end
