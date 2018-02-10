5b079364afc6c808dde0872cc72d8ff02b78e9f5
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:09:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,16 @@
 # Write your code here.]
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(names)
-  names.each do
-    puts "Hello, my name is '{name}'."
+  names.each {"Hello, my name is |name|"  }
+    #puts "Hello, my name is '{name}'."
+    names
 end
 end
+
+
+#def square_array(array)
+  # your code here
+#  squared = []
+#  array.each { |number| squared << number **2 }
+  #squared
+#end
