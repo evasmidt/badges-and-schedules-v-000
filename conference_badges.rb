6079e27b818657530fde26013022a301dd156011
Eva Smidt <shevangeline@gmail.com>
2018-02-10 18:26:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,14 @@ def batch_badge_creator(attendees)
   end
 
 end
-#end
+
+
+#Write a method called assign_rooms that takes the
+#list of speakers and assigns each speaker to a room.
+#Make sure that each room only has one speaker.
+#You have rooms 1-7.
+#return a list of room assignments in the form of:
+#"Hello, _____! You'll be assigned to room _____!"
 
 #def square_array(array)
   # your code here
